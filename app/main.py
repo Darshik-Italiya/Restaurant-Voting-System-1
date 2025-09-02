404afbe96592dd4026b6d27c808b0e55cfa8b833
Darshik-Italiya <dai.globalia@gmail.com>
2025-09-02 17:44:42 +0530

    comment

--- app/main.py
+++ app/main.py
@@ -12,3 +12,10 @@ app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(restaurants.router, prefix="/restaurants", tags=["restaurants"])
 app.include_router(votes.router, prefix="/votes", tags=["votes"])
 app.include_router(leaderboard.router, prefix="/leaderboard", tags=["leaderboard"])
+
+
+# DATABASE_URL = postgresql://postgres:%40Darshik123@localhost:5432/restaurant
+
+# SECRET_KEY = weresfsdfdgbvb56h5h5thgfhgj
+# ALGORITHM = "HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES = 60
