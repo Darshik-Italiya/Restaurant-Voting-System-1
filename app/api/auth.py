bc520ae9b3691b3965373b75bba375d9c989abac
janvichovatiya <jac.globalia@gmail.com>
2025-09-03 16:04:21 +0530

    create login schema

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from app.schemas.user import UserCreate, Token, UserOut
+from app.schemas.user import UserCreate, Token, UserOut, UserLogin
 from app.crud import user as crud_user
 from app.database import get_db
 from app.core.security import verify_password, create_access_token
@@ -16,9 +16,11 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @router.post("/login", response_model=Token)
-def login(form_data: UserCreate, db: Session = Depends(get_db)):
+def login(form_data: UserLogin, db: Session = Depends(get_db)):
     db_user = crud_user.get_user_by_email(db, form_data.email)
-    if not db_user or not verify_password(form_data.password, str(db_user.hashed_password)):
+    if not db_user or not verify_password(
+        form_data.password, str(db_user.hashed_password)
+    ):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
         )
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -7,6 +7,11 @@ class UserCreate(BaseModel):
     password: str
 
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
 class UserOut(BaseModel):
     id: int
     username: str
