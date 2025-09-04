ca6b9fd5acfa3630285b6888ee55c27be4342d1d
janvichovatiya <jac.globalia@gmail.com>
2025-09-04 12:13:25 +0530

    connect front-end

--- app/main.py
+++ app/main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 from app.api import auth, users, restaurants, votes
 from app.database import Base, engine
 from app.api import leaderboard
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Restaurant Voting System")
+
+origins = ["http://localhost:5174"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["Users"])
 # app.include_router(users.router, prefix="/users", tags=["users"])
